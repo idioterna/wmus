edcd39eeac327380223c08031db144663efe88f5
idiot <jure.koren@zemanta.com>
2015-02-19 17:33:45 +0100

    titles are utf-8

--- pafyurl.py
+++ pafyurl.py
@@ -4,7 +4,7 @@ import pafy, sys
 
 if len(sys.argv) == 2:
 	youtube = pafy.new(sys.argv[1])
-	print(youtube.title)
+	print(youtube.title.encode('utf-8'))
 	print(youtube.getbestaudio().url)
 else:
 	print("usage: %s <url>" % sys.argv[0], file=sys.stderr)
